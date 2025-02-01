7ce9cb905191c60b9553752486651422709f442a
DJ-LAMB <Chibusonma Udensi>
2025-02-01 18:07:31 +0100

    updated tab

--- MaesDining/settings.py
+++ MaesDining/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-u3)i-gj%pg*vgfg(js6dkqx!9=(_n+)i+8f(4hadol13ypvst5
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+# ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', *]
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
